d28b0db293a2aa4089d7cd68c89c1ecf6a5421af
Kapsyl6 <frejguste@gmail.com>
2025-01-04 16:32:59 +0100

    Initial commit

---
+++ hundregistret/src/Owner.java
@@ -0,0 +1,11 @@
+public class Owner {
+
+    public Owner(String name){
+        
+    }
+
+    public String getName(){
+        return "Henrik";
+    }
+
+}
---
+++ hundregistret/src/OwnerTestProgram.java
@@ -0,0 +1,6 @@
+public class OwnerTestProgram {  
+     public static void main(String[] args) {
+        Owner o = new Owner("Olle");
+        System.out.println(o.getName() );
+     }
+} 
